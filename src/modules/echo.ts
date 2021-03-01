eb30aa14be42a6c3776bbca02091688133ca77c2
YS Chung <hello@yschung.org>
2021-03-01 23:31:33 +0800

    Reject Promise instead of void if nothing happens in this module

--- src/modules/echo.ts
+++ src/modules/echo.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { ChatMessage } from "../types/ChatMessage";
 
-export async function echo(message: ChatMessage): Promise<ChatMessage | void> {
+export async function echo(message: ChatMessage): Promise<ChatMessage> {
     if (message.command === "!echo") {
         message.text = `Echo: ${message.text}`;
         return message;
     }
+
+    return Promise.reject();
 }
