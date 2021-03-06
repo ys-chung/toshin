cff1301defc02dc9041b463159554aeeedd5ec1f
YS Chung <hello@yschung.org>
2021-03-06 19:03:17 +0800

    Add emotes, extra comments and handle Promise.any errors properly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import Discord, { TextChannel } from "discord.js";
-import TelegramBot, { ParseMode } from "node-telegram-bot-api";
+import TelegramBot, { SendMessageOptions } from "node-telegram-bot-api";
 
 import { ConfigInterface } from "./types/ConfigInterface";
 import { ChatMessage } from "./types/ChatMessage"
@@ -11,6 +11,7 @@ import { generateFindRoom } from "./utils/findRoom";
 
 import { echo } from "./modules/echo";
 import { choose } from "./modules/choose";
+import { emotes } from "./modules/emotes";
 
 function readConfig(): ConfigInterface {
     try {
@@ -39,18 +40,34 @@ async function sendMessage(message: ChatMessage, discordClient: Discord.Client, 
     if (message.room.telegramId) {
         const escapedText = escapeTextFormat(message.text, BotType.Telegram);
         const text = message.italic ? `_${escapedText}_` : escapedText;
-        const parseMode: ParseMode | undefined = message.italic ? "MarkdownV2" : undefined;
-        await telegramBot.sendMessage(message.room.telegramId, text, { parse_mode: parseMode });
+        const options: SendMessageOptions = { parse_mode: "MarkdownV2" };
+        await telegramBot.sendMessage(message.room.telegramId, text, options);
     }
 
 }
 
 async function processCommand(message: ChatMessage, discordClient: Discord.Client, telegramBot: TelegramBot) {
-    const response = await Promise.any([
-        echo(message),
-        choose(message)
-    ]);
+    // Check if any of the commands return a result
+    let response;
 
+    try {
+        response = await Promise.any([
+            choose(message),
+            echo(message),
+            emotes(message)
+        ]);
+    } catch(error) {
+        const allErrors = error as AggregateError;
+        const allErrorsArray = allErrors.errors;
+        if (!(allErrorsArray.every((e) => e === undefined))) {
+            console.error(allErrorsArray);
+            throw new Error("Error when executing commands");
+        }
+    }
+
+    if (response === undefined) return;
+
+    // When one of them returns a result, send the message
     void sendMessage(response, discordClient, telegramBot);
 }
 
@@ -67,23 +84,36 @@ async function init() {
     const telegramBot = new TelegramBot(config.telegramToken, { polling: true });
     telegramBot.on("error", console.error);
 
+    // Create the find room function with rooms in config
     const findRoom = generateFindRoom(config.rooms);
 
+    // When Telegram bot recieves a message
     telegramBot.on("text", (message) => {
+        // Find which room the message is from, if configured
         const room = findRoom(String(message.chat.id));
 
+        // If a room is found
         if (room) {
+
+            // If the message could be a command starting with "/"
             if (message.text?.startsWith("/")) {
                 const text = message.text;
+
+                // Find the command name
                 const commandMatch = /^\/(\S*)/.exec(text);
 
+                // If no command name is found, return
                 if (!commandMatch || !commandMatch[1]) return;
 
                 const command = commandMatch[1];
-                const params = text.substring(commandMatch[0].length);
 
-                const incomingMessage: ChatMessage = { text, room, command, params };
+                // Set params as the message without the leading "/" and space
+                const params = text.substring(commandMatch[0].length + 1);
+                const sender = message.from?.username;
 
+                const incomingMessage: ChatMessage = { text, room, command, params, sender };
+
+                // Process the command
                 void processCommand(incomingMessage, discordClient, telegramBot);
             }
         }
