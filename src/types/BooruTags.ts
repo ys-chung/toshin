4ae3ddb4387ca15058a1d00c580c037c511b6c75
YS Chung <hello@yschung.org>
2021-12-02 09:02:53 +0000

    Booru types

---
+++ src/types/BooruTags.ts
@@ -0,0 +1,17 @@
+import _ from "lodash"
+
+type Tag = {
+    label: string,
+    value: string
+}
+
+export type BooruTags = Tag[]
+
+export function isBooruTags(candidate: unknown): candidate is BooruTags {
+    const predicate = candidate as BooruTags;
+
+    return (
+        _.isArray(predicate) &&
+        predicate.every(e => _.isString(e.value))
+    )
+}
