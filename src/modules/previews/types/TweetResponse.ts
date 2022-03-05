0ce8d735f22d72e475f1526ba8bbfd853459bb42
YS Chung <hello@yschung.org>
2022-03-05 22:50:54 +0800

    Update TweetResponse.ts

--- src/modules/previews/types/TweetResponse.ts
+++ src/modules/previews/types/TweetResponse.ts
@@ -10,7 +10,10 @@ interface TweetResponse {
         possibly_sensitive?: boolean
     }
     includes?: {
-        media?: { media_key: string; type: "animated_gif" | "photo" | "video" }[]
+        media?: {
+            media_key: string
+            type: "animated_gif" | "photo" | "video"
+        }[]
     }
 }
 export function isThisATweetResponse(candidate: unknown): candidate is TweetResponse {
