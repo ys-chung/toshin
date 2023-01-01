273470310e3c49828e13403991d861c1a84461f7
YS Chung <hello@yschung.org>
2023-01-01 23:04:38 +0800

    Refactor emotes

--- src/commands/emotes.ts
+++ src/commands/emotes.ts
@@ -23,163 +23,163 @@ import { Config, Emotes } from "../utils/Config.js"
 import { sample, isURL } from "../utils/utils.js"
 import { inApprovedGuild } from "../utils/guard.js"
 
+import { type SimpleEmote, type ReplacementEmote } from "../types/Emote.js"
+
+function simpleEmoteCommand(emoteName: string, emote: SimpleEmote) {
+  const commandOptions = {
+    name: emoteName.toLowerCase() as Lowercase<string>,
+    description: emoteName
+  }
+
+  @Discord()
+  class _SimpleEmoteCommand {
+    generateOptions() {
+      const selectedReply = Array.isArray(emote.content)
+        ? sample(emote.content)
+        : emote.content
+
+      if (isURL(selectedReply)) {
+        if (selectedReply.match(/\.(?:gif|jpg|png)$/))
+          return {
+            embeds: [new EmbedBuilder(baseEmbedJson).setImage(selectedReply)]
+          }
+
+        return { content: selectedReply }
+      }
+
+      return {
+        embeds: [new EmbedBuilder(baseEmbedJson).setDescription(selectedReply)]
+      }
+    }
+
+    @SimpleCommand(commandOptions)
+    replyCommand(command: SimpleCommandMessage) {
+      return command.message.reply(this.generateOptions())
+    }
+
+    @Slash(commandOptions)
+    @Guard(inApprovedGuild)
+    replyInteraction(i: CommandInteraction) {
+      return i.reply(this.generateOptions())
+    }
+  }
+}
+
+function replacementEmoteCommand(
+  emoteName: string,
+  replacementEmote: ReplacementEmote
+) {
+  const commandOptions = {
+    name: emoteName.toLowerCase() as Lowercase<string>,
+    description: emoteName + " a friend"
+  }
+
+  const commandOptionOptions = {
+    name: "friend" as const,
+    description: "cool friend to " + emoteName,
+    required: true
+  }
+
+  @Discord()
+  class _ReplacementEmoteCommand {
+    generateOptions(friend: string, sender: string) {
+      if (
+        replacementEmote.verifyParams &&
+        friend.replaceAll("\u200B", "") !== Config.commands.emotes.allowedParams
+      )
+        return
+
+      const selectedContent = sample(replacementEmote.content)
+
+      let replyText: string
+
+      if (selectedContent.length === 2) {
+        replyText = [selectedContent[0], friend, selectedContent[1]].join("")
+      } else if (selectedContent.length === 3) {
+        replyText = [
+          selectedContent[0],
+          friend,
+          selectedContent[1],
+          sender,
+          selectedContent[2]
+        ].join("")
+      } else if (selectedContent.length === 1) {
+        replyText = selectedContent[0]
+      } else {
+        throw new Error(
+          `Array of element in content of replacement emote "${emoteName}" is neither 1, 2 or 3 elements long.`
+        )
+      }
+
+      return {
+        embeds: [
+          new EmbedBuilder(baseEmbedJson).setDescription(
+            `_${Config.emoji}\n\n${replyText}_`
+          )
+        ]
+      }
+    }
+
+    @SimpleCommand(commandOptions)
+    replyCommand(
+      @SimpleCommandOption({
+        ...commandOptionOptions,
+        type: SimpleCommandOptionType.String
+      })
+      friend: string,
+      command: SimpleCommandMessage
+    ) {
+      if (!command.isValid()) {
+        return command.sendUsageSyntax()
+      }
+
+      const messageOptions = this.generateOptions(
+        cleanContent(friend, command.message.channel),
+        escapeMarkdown(
+          command.message.member?.nickname ?? command.message.author.username
+        )
+      )
+
+      if (!messageOptions) return
+
+      return command.message.reply(messageOptions)
+    }
+
+    @Slash(commandOptions)
+    @Guard(inApprovedGuild)
+    replyInteraction(
+      @SlashOption({
+        ...commandOptionOptions,
+        type: ApplicationCommandOptionType.String
+      })
+      friend: string,
+      i: CommandInteraction
+    ) {
+      if (!i.channel) return
+
+      const messageOptions = this.generateOptions(
+        cleanContent(friend, i.channel),
+        escapeMarkdown(
+          i.guild?.members.resolve(i.user.id)?.nickname ?? i.user.username
+        )
+      )
+
+      if (!messageOptions) return
+
+      return i.reply(messageOptions)
+    }
+  }
+}
+
 for (const [emoteName, emote] of Object.entries(Emotes)) {
   switch (emote.type) {
     case "simple": {
-      const simpleEmote = emote
-      const commandOptions = {
-        name: emoteName.toLowerCase() as Lowercase<string>,
-        description: emoteName
-      }
-
-      @Discord()
-      class _SimpleEmoteCommand {
-        generateOptions() {
-          const selectedReply = Array.isArray(simpleEmote.content)
-            ? sample(simpleEmote.content)
-            : simpleEmote.content
-
-          if (isURL(selectedReply)) {
-            if (selectedReply.match(/\.(?:gif|jpg|png)$/))
-              return {
-                embeds: [
-                  new EmbedBuilder(baseEmbedJson).setImage(selectedReply)
-                ]
-              }
-
-            return { content: selectedReply }
-          }
-
-          return {
-            embeds: [
-              new EmbedBuilder(baseEmbedJson).setDescription(selectedReply)
-            ]
-          }
-        }
-
-        @SimpleCommand(commandOptions)
-        replyCommand(command: SimpleCommandMessage) {
-          return command.message.reply(this.generateOptions())
-        }
-
-        @Slash(commandOptions)
-        @Guard(inApprovedGuild)
-        replyInteraction(i: CommandInteraction) {
-          return i.reply(this.generateOptions())
-        }
-      }
-
+      simpleEmoteCommand(emoteName, emote)
       break
     }
 
     case "replacement": {
-      const replacementEmote = emote
-
-      const commandOptions = {
-        name: emoteName.toLowerCase() as Lowercase<string>,
-        description: emoteName + " a friend"
-      }
-
-      const commandOptionOptions = {
-        name: "friend" as const,
-        description: "cool friend to " + emoteName,
-        required: true
-      }
-
-      @Discord()
-      class _ReplacementEmoteCommand {
-        generateOptions(friend: string, sender: string) {
-          if (
-            replacementEmote.verifyParams &&
-            friend.replaceAll("\u200B", "") !==
-              Config.commands.emotes.allowedParams
-          )
-            return
-
-          const selectedContent = sample(replacementEmote.content)
-
-          let replyText: string
-
-          if (selectedContent.length === 2) {
-            replyText = [selectedContent[0], friend, selectedContent[1]].join(
-              ""
-            )
-          } else if (selectedContent.length === 3) {
-            replyText = [
-              selectedContent[0],
-              friend,
-              selectedContent[1],
-              sender,
-              selectedContent[2]
-            ].join("")
-          } else if (selectedContent.length === 1) {
-            replyText = selectedContent[0]
-          } else {
-            throw new Error(
-              `Array of element in content of replacement emote "${emoteName}" is neither 1, 2 or 3 elements long.`
-            )
-          }
-
-          return {
-            embeds: [
-              new EmbedBuilder(baseEmbedJson).setDescription(
-                `_${Config.emoji}\n\n${replyText}_`
-              )
-            ]
-          }
-        }
-
-        @SimpleCommand(commandOptions)
-        replyCommand(
-          @SimpleCommandOption({
-            ...commandOptionOptions,
-            type: SimpleCommandOptionType.String
-          })
-          friend: string,
-          command: SimpleCommandMessage
-        ) {
-          if (!command.isValid()) {
-            return command.sendUsageSyntax()
-          }
-
-          const messageOptions = this.generateOptions(
-            cleanContent(friend, command.message.channel),
-            escapeMarkdown(
-              command.message.member?.nickname ??
-                command.message.author.username
-            )
-          )
-
-          if (!messageOptions) return
-
-          return command.message.reply(messageOptions)
-        }
-
-        @Slash(commandOptions)
-        @Guard(inApprovedGuild)
-        replyInteraction(
-          @SlashOption({
-            ...commandOptionOptions,
-            type: ApplicationCommandOptionType.String
-          })
-          friend: string,
-          i: CommandInteraction
-        ) {
-          if (!i.channel) return
-
-          const messageOptions = this.generateOptions(
-            cleanContent(friend, i.channel),
-            escapeMarkdown(
-              i.guild?.members.resolve(i.user.id)?.nickname ?? i.user.username
-            )
-          )
-
-          if (!messageOptions) return
-
-          return i.reply(messageOptions)
-        }
-      }
-
+      replacementEmoteCommand(emoteName, emote)
       break
     }
   }
--- src/types/Emote.ts
+++ src/types/Emote.ts
@@ -7,14 +7,16 @@ const SimpleEmoteSchema = z.object({
   content: z.union([z.string(), z.array(z.string())])
 })
 
+export type SimpleEmote = z.infer<typeof SimpleEmoteSchema>
+
 const ReplacementEmoteSchema = z.object({
   type: z.literal("replacement"),
   content: z.array(z.array(z.string())),
   verifyParams: z.boolean().optional()
 })
 
-const EmoteSchema = SimpleEmoteSchema.or(ReplacementEmoteSchema)
+export type ReplacementEmote = z.infer<typeof ReplacementEmoteSchema>
 
-export type Emote = z.infer<typeof EmoteSchema>
+const EmoteSchema = SimpleEmoteSchema.or(ReplacementEmoteSchema)
 
 export const EmoteListSchema = z.record(EmoteSchema)
