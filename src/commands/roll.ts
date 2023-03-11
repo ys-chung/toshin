a0bbe28a15950352c74e0b9e44c1099ef8391597
YS Chung <hello@yschung.org>
2023-03-11 15:48:38 +0800

    Add !dice

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,12 +1,29 @@
-import { EmbedBuilder } from "@discordjs/builders"
-import { Dice } from "@scio/dice-typescript"
+import {
+  Discord,
+  SimpleCommand,
+  type SimpleCommandMessage,
+  SimpleCommandOption,
+  SimpleCommandOptionType,
+  Slash,
+  SlashOption
+} from "discordx"
 
-import { ToshinCommand } from "../utils/ToshinCommand.js"
+import {
+  ApplicationCommandOptionType,
+  type CommandInteraction,
+  EmbedBuilder
+} from "discord.js"
+
+import { Dice } from "@scio/dice-typescript"
+import { z } from "zod"
+
+import { ToshinCommand, baseEmbedJson } from "../utils/ToshinCommand.js"
 import { Log } from "../utils/log.js"
 
 import { Config } from "../utils/Config.js"
 
 const log = new Log("roll")
+const dice = new Dice()
 
 @ToshinCommand({
   name: "roll",
@@ -19,11 +36,9 @@ const log = new Log("roll")
   }
 })
 export class RollCommand {
-  readonly dice = new Dice()
-
   answer(paramString: string) {
     try {
-      const result = this.dice.roll(paramString)
+      const result = dice.roll(paramString)
       return new EmbedBuilder().setDescription(
         result.errors.length > 0
           ? `${Config.emoji}\n\nError: ${result.errors[0].message}`
@@ -33,5 +48,68 @@ export class RollCommand {
       log.error("Roll error", error)
       return new EmbedBuilder().setDescription("Error: Roll failed")
     }
+  }
+}
+
+@Discord()
+export class DiceCommand {
+  @SimpleCommand({
+    name: "dice",
+    description: "roll a die"
+  })
+  simpleReply(
+    @SimpleCommandOption({
+      name: "sides",
+      type: SimpleCommandOptionType.Number,
+      description: "number of sides of the die"
+    })
+    sides = 6,
+    command: SimpleCommandMessage
+  ) {
+    void log.info("Got simple command", sides)
+
+    if (z.number().safeParse(sides).success === false) {
+      void log.info("Simple command invalid, replying usage syntax")
+      return command.sendUsageSyntax()
+    }
+
+    const result = dice.roll(`d${sides}`)
+    void log.info("Die rolled, replying embed")
+
+    return command.message.reply({
+      embeds: [
+        new EmbedBuilder(baseEmbedJson).setDescription(
+          `${Config.emoji} rolls 🎲 ...\n\n**👉 ${result.total}! 👈**`
+        )
+      ]
+    })
+  }
+
+  @Slash({
+    name: "dice",
+    description: "roll a die"
+  })
+  slashReply(
+    @SlashOption({
+      name: "sides",
+      type: ApplicationCommandOptionType.Number,
+      description: "number of sides of the die",
+      required: false
+    })
+    sides = 6,
+    i: CommandInteraction
+  ) {
+    void log.info("Got slash command", sides)
+
+    const result = dice.roll(`d${sides}`)
+    void log.info("Die rolled, replying embed")
+
+    return i.reply({
+      embeds: [
+        new EmbedBuilder(baseEmbedJson).setDescription(
+          `${Config.emoji} rolls 🎲 ...\n\n**👉 ${result.total}! 👈**`
+        )
+      ]
+    })
   }
 }
