5daaf61261c8f0d5428d83394962dfbd2e627805
YS Chung <hello@yschung.org>
2025-01-21 20:52:06 +0800

    add custom booru cmds

--- commands/booru/cmd.ts
+++ commands/booru/cmd.ts
@@ -1,9 +1,63 @@
 import { forSite } from "booru"
 
-import { defineCmd } from "../../util/Cmd"
+import { defineCmd, type FlatParamsCmdInteraction } from "../../util/Cmd"
 import { ToshinEmbedBuilder } from "../../util/ToshinEmbedBuilder"
+import { Config } from "../../util/Config"
 
 const Sb = forSite("sb")
+
+async function runBooru(
+  interaction: FlatParamsCmdInteraction,
+  fixedTags?: string
+) {
+  if (interaction.paramString.length === 0 && !fixedTags) {
+    console.log("no tags provided")
+    interaction.reply({
+      embeds: [new ToshinEmbedBuilder().setDescription("no tags provided")]
+    })
+    return
+  }
+
+  const searchString =
+    interaction.paramString + (fixedTags ? ` ${fixedTags}` : "")
+
+  const result = await Sb.search(searchString, {
+    limit: 1,
+    random: true
+  })
+
+  console.log("got result")
+
+  if (result.length === 0) {
+    console.log("no images found")
+    interaction.reply({
+      embeds: [new ToshinEmbedBuilder().setDescription("no images found")]
+    })
+    return
+  }
+
+  const post = result[0]
+  const imageUrl = post.sampleUrl ?? post.fileUrl ?? post.previewUrl
+
+  if (imageUrl === null) {
+    console.log("no image url found")
+    interaction.reply({
+      embeds: [new ToshinEmbedBuilder().setDescription("no images found")]
+    })
+    return
+  }
+
+  console.log("got image url")
+
+  interaction.reply({
+    embeds: [
+      new ToshinEmbedBuilder()
+        .setImage(imageUrl)
+        .setURL(post.postView)
+        .setTitle("post")
+    ]
+  })
+}
 
 for (const booruName of ["sb", "db"] as const) {
   defineCmd({
@@ -17,53 +71,24 @@ for (const booruName of ["sb", "db"] as const) {
       }
     ],
     flatParams: true,
-    run: async (interaction) => {
-      console.log(booruName)
-
-      if (interaction.paramString.length === 0) {
-        console.log("no tags provided")
-        interaction.reply({
-          embeds: [new ToshinEmbedBuilder().setDescription("no tags provided")]
-        })
-        return
-      }
-
-      const result = await Sb.search(interaction.paramString, {
-        limit: 1,
-        random: true
-      })
-
-      console.log("got result")
-
-      if (result.length === 0) {
-        console.log("no images found")
-        interaction.reply({
-          embeds: [new ToshinEmbedBuilder().setDescription("no images found")]
-        })
-        return
-      }
-
-      const post = result[0]
-      const imageUrl = post.sampleUrl ?? post.fileUrl ?? post.previewUrl
-
-      if (imageUrl === null) {
-        console.log("no image url found")
-        interaction.reply({
-          embeds: [new ToshinEmbedBuilder().setDescription("no images found")]
-        })
-        return
-      }
-
-      console.log("got image url")
-
-      interaction.reply({
-        embeds: [
-          new ToshinEmbedBuilder()
-            .setImage(imageUrl)
-            .setURL(post.postView)
-            .setTitle("post")
-        ]
-      })
-    }
+    run: runBooru
   })
+}
+
+if (Config.booru) {
+  for (const [alias, tag] of Object.entries(Config.booru)) {
+    defineCmd({
+      name: alias.toLowerCase(),
+      description: `find a ${alias}`,
+      params: [
+        {
+          name: "tags",
+          description: "extra tags to search for",
+          required: false
+        }
+      ],
+      flatParams: true,
+      run: async (interaction) => runBooru(interaction, tag)
+    })
+  }
 }
--- util/Config.ts
+++ util/Config.ts
@@ -7,6 +7,7 @@ const Config = z
     token: z.string(),
     emoji: z.string(),
     colour: z.number(),
+    booru: z.record(z.string(), z.string()).optional(),
     emotes: z.object({
       simple: z.record(z.string().or(z.array(z.string()))),
       replacement: z.record(z.array(z.array(z.string()).min(1).max(3)))
